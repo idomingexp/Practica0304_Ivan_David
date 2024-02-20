01d277096a24555b4dcc8f702e11f0aca50e9f03
idomingexp <idomingexp@educacion.navarra.es>
2024-02-20 12:52:42 +0100

    Update Ejercicio 4.py

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,8 +1,36 @@
 import pygame
-from random import randint, uniform
+from random import randint
 
+#Nos indica la separación entre los ladrillos
+hueco_x = 600 / 6 + 600 / 36
+#creamos la clase ladrillo
+class Ladrillo:
+    def __init__(self, imagen, filas, columnas):
+        self.__imagen = pygame.image.load(imagen)
+        self.__rect = self.__imagen.get_rect()
+        self.__ladrillos = []
+
+        for y in range(filas):
+            ladrilloY = y * (self.__rect.height + 20)
+            for x in range(columnas):
+                ladrilloX = x * (self.__rect.width + hueco_x)
+                self.__ladrillos.append((ladrilloX, ladrilloY))
+
+    @property
+    def imagen(self):
+        return self.__imagen
+    
+    @imagen.setter
+    def imagen(self, valor):
+        self.__imagen = valor
+
+    def dibujar_ladrillos(self, ventana):
+        for pos in self.__ladrillos:
+            ventana.blit(self.__imagen, pos)
+
+# Inicialización de Pygame
 pygame.init()
-ventana = pygame.display.set_mode((1200,673))
+ventana = pygame.display.set_mode((1200, 673))
 pygame.display.set_caption("Ejemplo 4")
 
 #Cargamos el fondo de la ventana
@@ -13,8 +41,8 @@ ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 #Nos introduce una velocidad distinta de la pelota
 #en cada ejecución del juego
-speed = [randint(2,5),randint(2,5)]
-ballrect.move_ip(0,0)
+speed = [0,0]
+ballrect.move_ip(600,336)
 
 barra = pygame.image.load("bate.png")
 barrarect = barra.get_rect()
@@ -23,23 +51,17 @@ golpes = 4
 golpes_barra = 0
 fuente = pygame.font.Font(None, 36)
 
-ladrillo1 = pygame.image.load("Ladrillo1.png")
-ladrillo1rect = ladrillo1.get_rect()
-ladrillos = []
-filas = 6
-columnas = 6
-for y in range(filas):
-    ladrilloY = y * ladrillo1rect[3]
-    for x in range(columnas):
-        ladrilloX = x * ladrillo1rect[2]
-        ladrillos.append((ladrilloX, ladrilloY))
+# Crear instancia de la clase Ladrillo
+ladrillo = Ladrillo("Ladrillo1.png", 6, 6)
 
 jugando = True
 while jugando:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and speed == [0, 0]:  # Si se presiona la barra espaciadora y la bola está quieta
+                speed = [randint(2, 4), randint(2, 4)]  # Asigna una velocidad aleatoria a la bola
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         barrarect = barrarect.move(-4,0)
@@ -48,12 +70,12 @@ while jugando:
 
     if barrarect.colliderect(ballrect):
         speed[1] = -speed[1]
-        speed[1] *= 1.2
+        speed[1] *= 1.05
         #Aquí hacemos que cada 4 golpes la velocidad de la bola
-        #aumente un 20 por ciento 
+        #aumente un 5 por ciento 
         golpes_barra += 1
         if golpes_barra % golpes == 0:
-            speed[0] *= 1.2
+            speed[0] *= 1.05
 
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
@@ -72,10 +94,16 @@ while jugando:
         ventana.blit(texto, [texto_x, texto_y])
         
     else:
-        
         ventana.blit(fondo, (0,0))
-        for l in ladrillos:
-            ventana.blit(ladrillo1, l)
+        #Aquí tenemos las colisiones entre los ladrillos y la bola
+        for i, pos in enumerate(ladrillo._Ladrillo__ladrillos):
+            ladrillo_rect = pygame.Rect(pos, (ladrillo._Ladrillo__rect.width, ladrillo._Ladrillo__rect.height))
+            if ladrillo_rect.colliderect(ballrect):
+                speed[1] = -speed[1]
+                # Eliminar el ladrillo colisionado
+                del ladrillo._Ladrillo__ladrillos[i]
+                break
+        ladrillo.dibujar_ladrillos(ventana)
         ventana.blit(ball, ballrect)
         ventana.blit(barra, barrarect)
 
